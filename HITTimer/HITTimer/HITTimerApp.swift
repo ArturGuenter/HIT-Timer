0564b50e53dd5c49682073c1cf41616bb9f3e4d7
ArturGuenter <152178843+ArturGuenter@users.noreply.github.com>
2025-03-02 23:50:22 +0100

    Add files via upload

---
+++ HITTimer/HITTimer/HITTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HITTimerApp.swift
+//  HITTimer
+//
+//  Created by Artur Günter on 02.03.25.
+//
+
+import SwiftUI
+
+@main
+struct HITTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HITTimer/HITTimerTests/HITTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HITTimerTests.swift
+//  HITTimerTests
+//
+//  Created by Artur Günter on 02.03.25.
+//
+
+import Testing
+@testable import HITTimer
+
+struct HITTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
